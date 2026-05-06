3681c1cdd4429e2dc9cd060ae3a51e1613d798f1
Ivan Sambrana <ivansambrana@juscorrientes.gov.ar>
2026-05-06 12:48:25 -0300

    Update Saint Seiya card name to "Cosmo Convergence" in Lua script; mark Windbot subproject as dirty; update binary file for expansions

--- script/unofficial/c922100082.lua
+++ script/unofficial/c922100082.lua
@@ -1,4 +1,4 @@
---Athena Exclamation (Counter Trap)
+--Cosmo Convergence
 --[==[
 -- ID: 922100082
 -- Type: Trap / Counter Trap
@@ -9,7 +9,7 @@
 -- Effect (EN):
 -- If you control 3 or more "Saint" monsters with different names: When your opponent activates a card or effect: Negate the activation, and if you do, destroy that card.
 --]==]
---Athena Exclamation (Counter Trap)
+--Cosmo Convergence
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
